783116ad2307a9488dd625da0d76230e6f4d2cd0
BDalheimer <bernhard.dalheimer@hotmail.com>
2016-12-12 21:36:11 +0100

    seperated LH function from main function

---
+++ R/likelihoodFunction.R
@@ -0,0 +1,14 @@
+# likelihood function to optimize
+LH <- function(S, Tob, TB, Sigma_hat1, k,  Sigma_hat2) {
+
+  W <- matrix(S[1:(k*k)], nrow = k)
+  Psi <- diag(S[(k*k+1):(k*k+k)])
+  MW <- det(W %*% t(W))
+  MW2 <- det(W %*% Psi %*% t(W))
+  MMM <- W %*% t(W)
+  MMM2 <- W %*% Psi %*% t(W)
+  L <- -(((TB - 1) / 2) * (log(MW) + sum(diag((Sigma_hat1 %*% solve(MMM)))))) -
+    (((Tob - TB + 1) / 2) * (log(MW2) + sum(diag((Sigma_hat2 %*% solve(MMM2))))))
+
+  return(-L)
+}
