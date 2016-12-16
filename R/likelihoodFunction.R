0ad5081f518ded2a614d442467743809b6fd5950
alexanderlange53 <alex.lange_@gmx.de>
2016-12-16 21:12:09 +0100

    preventing the function to crash

--- R/likelihoodFunction.R
+++ R/likelihoodFunction.R
@@ -7,8 +7,13 @@ LH <- function(S, Tob, TB, Sigma_hat1, k,  Sigma_hat2) {
   MW2 <- det(W %*% Psi %*% t(W))
   MMM <- W %*% t(W)
   MMM2 <- W %*% Psi %*% t(W)
-  L <- -(((TB - 1) / 2) * (log(MW) + sum(diag((Sigma_hat1 %*% solve(MMM)))))) -
-    (((Tob - TB + 1) / 2) * (log(MW2) + sum(diag((Sigma_hat2 %*% solve(MMM2))))))
 
+  if(MW > 0 & MW2 > 0){
+    L <- -(((TB - 1) / 2) * (log(MW) + sum(diag((Sigma_hat1 %*% solve(MMM)))))) -
+      (((Tob - TB + 1) / 2) * (log(MW2) + sum(diag((Sigma_hat2 %*% solve(MMM2))))))
   return(-L)
+  }else{
+    return(NA)
+  }
+
 }
