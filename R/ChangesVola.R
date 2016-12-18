82a3779c3e7dbaf30575aa35cd92ec57d1140dc2
alexanderlange53 <alex.lange_@gmx.de>
2016-12-18 16:04:35 +0100

    ordering the estimated matrices with respect to largest diagonal elemnts

--- R/ChangesVola.R
+++ R/ChangesVola.R
@@ -50,10 +50,49 @@ ChangesVola <- function(x, SB){
     B.SE <- matrix(FishObs[1:(k*k)], k, k)
     Lambda.SE <- FishObs[(k*k+1):(k*k+k)]*diag(k)
   }
+
+    # ordering the columns with respect to the largest absolute values in each column
+    B_hat_ord <- matrix(0, k, k)
+    control <- rep(0, k)
+    for(i in 1:ncol(B_hat)){
+      for(j in 1:ncol(B_hat)){
+        if(which.max(abs(B_hat[, j])) == i){
+          if(control[i] == 0){
+            control[i] <- j
+            B_hat_ord[, i] <-B_hat[, j]
+          }else{
+            if(max(B_hat[, j]) > max(B_hat[, control[i]])){
+              control[i] <- j
+              B_hat_ord[, i] <-B_hat[, j]
+            }
+          }
+        }
+      }
+    }
+
+    # checking if any column in the orderd matrix is empty and replace it with the unused column in the estimated matrix
+    if(any(control == 0)){
+      hc <- apply(B_hat_ord, 2, function(x){all(x == 0)})
+      B_hat_ord[, which(hc == TRUE)] <- B_hat[, which(hc == TRUE)]
+      control[which(control == 0)] <- which(hc == TRUE)
+    }
+
+    # checking for negative values on the main daigonal
+    for(i in 1:ncol(B_hat_ord)){
+      if(B_hat_ord[i,i] < 0){
+        B_hat_ord[, i] <- B_hat_ord[, i]*(-1)
+      }
+    }
+
+    # reordering the lambda and S.E. matrices in the same way
+    B.SE <- B.SE[, control]
+    Lambda_hat <- diag(diag(Lambda_hat[, control]))
+    Lambda.SE <- diag(diag(Lambda.SE[, control]))
+
   return(list(
     Lambda = Lambda_hat,
     Lambda_SE = Lambda.SE,
-    B = B_hat,
+    B = B_hat_ord,
     B_SE = B.SE,
     n = Tob,
     InvFish = HESS,
