28306c148119a46ffe20276d8fb923df7a8c0254
BDalheimer <bernhard.dalheimer@hotmail.com>
2016-12-25 21:27:41 +0100

    Function which provides SB from date inputs #2

---
+++ R/getStructuralBreak.R
@@ -0,0 +1,22 @@
+# This internal function provides a numerical value for the structural break based on time input by user
+
+getStructuralBreak <- function(SB, dateVector = NULL, start = NULL, end = NULL,
+                                   frequency = NULL, format = NULL){
+
+if(is.null(dateVector) & is.null(start) & is.null(end)){
+
+  stop("Please provide either a valid number of observation or proper date specifications")
+}
+
+if(is.null(dateVector)){
+
+       dateVector =  seq(as.Date(start, format = format), as.Date(end, format = format),  frequency)
+  }
+
+    return(which(grepl(as.Date(SB, format = format), dateVector)))
+  }
+
+
+
+}
+}
