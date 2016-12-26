13179ecf1ce2f48a71e8e11eb3219dd9f0eda212
BDalheimer <bernhard.dalheimer@hotmail.com>
2016-12-26 01:11:54 +0100

    augment wald test to n dimensions

--- R/Wald.test.R
+++ R/Wald.test.R
@@ -3,22 +3,23 @@
 
 Wald.test <- function(Lambda, Sigma){
 
-  beta <- diag(Lambda)[1:2]
-  sigma <- diag(Sigma[10:11,10:11])
-  covS <- Sigma[10:11, 10:11][1,2]
-  l1_l2 <- round(1 - pchisq((beta%*%c(1, -1))^2/(sum(sigma) - 2*covS), 1), 2)
+  k = length(diag(Lambda))
 
-  beta <- diag(Lambda)[c(1,3)]
-  sigma <- diag(Sigma[c(10,12),c(10,12)])
-  covS <- Sigma[c(10,12),c(10,12)][1,2]
-  l1_l3 <- round(1 - pchisq((beta%*%c(1, -1))^2/(sum(sigma) - 2*covS), 1), 2)
+  kList = combn(1:k, 2)
 
-  beta <- diag(Lambda)[c(2,3)]
-  sigma <- diag(Sigma[c(11,12),c(11,12)])
-  covS <- Sigma[c(11,12),c(11,12)][1,2]
-  l2_l3 <- round(1 - pchisq((beta%*%c(1, -1))^2/(sum(sigma) - 2*covS), 1), 2)
+  sigmas = apply(kList, 2, function(x){
+    diag(Sigma[c(k * k + x[1],k*k + x[2]),c(k * k + x[1],k*k + x[2])])
+    })
 
-  return(c(l1_l2, l1_l3, l2_l3))
+  covS =  apply(kList, 2,
+                function(x){ Sigma[c(k * k + x[1],k*k + x[2]),c(k * k + x[1],k*k + x[2])][1,2]
+                  })
+
+  testRes = lapply(1:k, function(x){
+    round(1 - pchisq((betas[, x]%*%c(1, -1))^2/(sum(sigmas[,x]) - 2*covS[x]), 1), 2)
+    })
+
+  return(unlist(testRes))
 }
 
 
