1d231e574adeac63e1b423dc5ce9038d4a201806
alexanderlange53 <alex.lange_@gmx.de>
2016-12-29 20:07:45 +0100

    iterative gls estimation. This closes #6

--- R/id.cv.R
+++ R/id.cv.R
@@ -25,7 +25,7 @@
 
 
 id.cv <- function(x, SB, start = NULL, end = NULL, frequency = NULL,
-                        format = NULL, dateVector = NULL){
+                        format = NULL, dateVector = NULL, max.iter = 10, crit = 0.05){
 
   if(!is.numeric(SB)){
 
@@ -61,8 +61,91 @@ id.cv <- function(x, SB, start = NULL, end = NULL, frequency = NULL,
     B_hat <- matrix(MLE$par[1:(k*k)], nrow = k)
     Lambda_hat <- diag(MLE$par[(k*k+1):(k*k+k)])
 
+    # estimating again with GLS to obatin a more precise estimation
+    y <- t(x$y)
+
+    Z_t <- matrix(0, nrow(y)*p, ncol(y))
+    for(i in 1:ncol(y)-p){
+      Z_t[,i] <- c(y[, ((i+1):(i+p))])
+    }
+
+    gls1 <- function(Z, Sig){
+      G <- kronecker(tcrossprod(Z), Sig)
+      return(G)
+    }
+
+    resid.gls <- function(Z_t, k, GLS_hat){
+      term1 <- kronecker(t(Z_t), diag(k))%*%GLS_hat
+      return(term1)
+    }
+
+    Lambda_hat <- list(Lambda_hat)
+    B_hat <- list(B_hat)
+
+    counter <- 1
+    Exit <- 1
+
+    while(abs(Exit) > crit & counter < max.iter){
+
+      Sig1 <- solve(tcrossprod(B_hat[[counter]]))
+      Sig2 <- solve(B_hat[[counter]]%*%tcrossprod(Lambda_hat[[counter]], B_hat[[counter]]))
+
+      GLS1.1 <- rowSums(apply(Z_t[, 1:(TB-1)], 2, gls1, Sig = Sig1))
+      GLS1.2 <- rowSums(apply(Z_t[, (TB):ncol(Z_t)], 2, gls1, Sig = Sig2))
+      GLS1 <- solve(matrix(GLS1.1 + GLS1.2, nrow = k*k*p, byrow = F))
+
+      GLS2.1 <- matrix(0, nrow = k*k*p, ncol = (TB-1))
+      GLS2.2 <- matrix(0, nrow = k*k*p, ncol = ncol(y))
+
+      for(i in 1:(TB-1)){
+        GLS2.1[,i] <- kronecker(Z_t[,i], Sig1)%*%y[,i]
+      }
+      for(i in TB:ncol(y)){
+        GLS2.2[,i] <- kronecker(Z_t[,i], Sig2)%*%y[,i]
+      }
+
+
+      GLS2.1 <- rowSums(GLS2.1)
+      GLS2.2 <- rowSums(GLS2.2)
+      GLS2 <- GLS2.1 + GLS2.2
+
+      GLS_hat <- GLS1%*%GLS2
+
+      term1 <- apply(Z_t, 2, resid.gls, k = k, GLS_hat = GLS_hat)
+      u_tgls <- t(y) - t(term1)
+
+      resid1gls <- u_tgls[1:TB-1,]
+      resid2gls <- u_tgls[TB:Tob,]
+      Sigma_hat1gls <- (crossprod(resid1gls)) / (TB-1)
+      Sigma_hat2gls <- (crossprod(resid2gls)) / (Tob-TB+1)
+
+      # Determine starting values for B and Lambda
+      B <- expm::sqrtm((1/Tob)* crossprod(u_tgls)) + matrix(runif(k*k), nrow = k, byrow = T)
+      Lambda <- diag(Lambda_hat[[counter]])
+      S <- c(cbind(B, Lambda))
+
+      # optimize the likelihood function
+      MLEgls <- optim(fn = LH, par = S, k = k, TB = TB, Sigma_hat1 = Sigma_hat1gls,
+                      Sigma_hat2 = Sigma_hat2gls, Tob = Tob, method = 'L-BFGS-B', hessian = T)
+
+
+
+      B_hatg <- matrix(MLEgls$par[1:(k*k)], nrow = k)
+      Lambda_hatg <- diag(MLEgls$par[(k*k+1):(k*k+k)])
+
+      B_hat <- c(B_hat, list(B_hatg))
+      Lambda_hat <- c(Lambda_hat, list(Lambda_hatg))
+
+      counter <- counter +1
+      Exit <- sum(diag(Lambda_hat[[counter]])) - sum(diag(Lambda_hat[[counter - 1]]))
+    }
+
+    # extracting the last estimates
+    B_hat <- B_hat[[counter]]
+    Lambda_hat <- Lambda_hat[[counter]]
+
     # obtaining standard errors from inverse fisher information matrix
-    HESS <- solve(MLE$hessian)
+    HESS <- solve(MLEgls$hessian)
 
     for(i in 1:nrow(HESS)){
       if(HESS[i,i] < 0){
