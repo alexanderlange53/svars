f10c7b9704f35734a2d310ba6482ad1b89f94b6b
Bernhard Dalheimer <bernhard.dalheimer@hotmail.com>
2017-03-03 17:39:28 +0100

    Make ic.cv great again

--- R/id.cv.R
+++ R/id.cv.R
@@ -6,7 +6,7 @@
 #' @param SB integer. Structural break either of type integer as the number of observations which belong to the pre-break period or
 #'                    Date character. If a date character is provided, either a date Vector which contains the time line of the data
 #'                    in corresponding format or then the conventional time parameters need to be provided.
-#' @param dateVector vector. Vector of the time period concerned containing SB
+#' @param dateVector vector. Vector of the time period concerned containing SB.
 #' @param start character. Start of the time series (only if dateVector is empty)
 #' @param end character. End of the time series (only if dateVector is empty)
 #' @param frequency character. Frequency of the time series (only if dateVector is empty)
@@ -43,14 +43,12 @@
 #' x1 <- id.cv(v1, SB = 60)
 #' summary(x1)
 #'
-#' # switching columns according to sign patter
+# switching columns according to sign patter
 #' x1$B <- x1$B[,c(3,2,1)]
 #' x1$B[,3] <- x1$B[,3]*(-1)
 #'
-#' # Impulse response analysis
-#' ira <- irf(x1, horizon = 20)
-#'
-#' plot(ira, scales = 'free_y')
+# Impulse response analysis
+#' plot(x1, horizon = 30, scales = 'free_y')
 #' }
 #'
 #' @export
@@ -103,19 +101,27 @@ id.cv <- function(x, SB, start = NULL, end = NULL, frequency = NULL,
    # Determine starting values for B and Lambda
    MLE <- NULL
    counter2 <- 0
+
    while(is.null(MLE) & counter2 < 500){
      MW <- -1
      while(MW < 0.5){
        B <- suppressMessages(expm::sqrtm((1/Tob)* crossprod(u_t))) + matrix(runif(k*k), nrow = k, byrow = T)
        MW <- det(tcrossprod(B))
      }
+
+     B <- c(B)
+     if(!is.null(restriction_matrix)){
+       restrictions <- length(restriction_matrix[!is.na(restriction_matrix)])
+       B <- B[1:(length(B)-restrictions)]
+     }
      Lambda <- c(1,1,1)
-     S <- c(cbind(B, Lambda))
+     S <- c(B, Lambda)
+
 
      # optimize the likelihood function
      MLE <- tryCatch(
        optim(fn = LH, par = S, k = k, TB = TB, Sigma_hat1 = Sigma_hat1,
-             Sigma_hat2 = Sigma_hat2, Tob = Tob, method = 'L-BFGS-B', hessian = T, restriction_matrix = restriction_matrix),
+             Sigma_hat2 = Sigma_hat2, Tob = Tob, method = "L-BFGS-B", hessian = T, restriction_matrix = restriction_matrix),
        error = function(e) NULL)
      counter2 <- counter2 + 1
      if(counter2 == 500){
@@ -123,181 +129,195 @@ id.cv <- function(x, SB, start = NULL, end = NULL, frequency = NULL,
      }
    }
 
-    B_hat <- matrix(MLE$par[1:(k*k)], nrow = k)
-    Lambda_hat <- diag(MLE$par[(k*k+1):(k*k+k)])
-    ll <- MLE$value
+   if(!is.null(restriction_matrix)){
+     naElements <- is.na(restriction_matrix)
+     restriction_matrix[naElements] <- MLE$par[1:sum(naElements)]
+     B_hat <- restriction_matrix
+     Lambda_hat <- diag(MLE$par[sum(naElements):length(MLE$par)])
+   }else{
+     B_hat <- matrix(MLE$par[1:(k*k)], nrow = k)
+     Lambda_hat <- diag(MLE$par[(k*k+1):(k*k+k)])
+   }
+   ll <- MLE$value
 
-    # estimating again with GLS to obatin a more precise estimation
-    y <- t(x$y)
+   # estimating again with GLS to obatin a more precise estimation
+   y <- t(x$y)
 
-    yl <- t(y_lag_cr(t(y), p)$lags)
-    y <- y[,-c(1:p)]
+   yl <- t(y_lag_cr(t(y), p)$lags)
+   y <- y[,-c(1:p)]
 
-    Z_t <- rbind(rep(1, ncol(yl)), yl)
+   Z_t <- rbind(rep(1, ncol(yl)), yl)
 
-    gls1 <- function(Z, Sig){
-      G <- kronecker(tcrossprod(Z), Sig)
-      return(G)
-    }
+   gls1 <- function(Z, Sig){
+     G <- kronecker(tcrossprod(Z), Sig)
+     return(G)
+   }
 
-    resid.gls <- function(Z_t, k, GLS_hat){
-      term1 <- kronecker(t(Z_t), diag(k))%*%GLS_hat
-      return(term1)
-    }
+   resid.gls <- function(Z_t, k, GLS_hat){
+     term1 <- kronecker(t(Z_t), diag(k))%*%GLS_hat
+     return(term1)
+   }
 
-    Lambda_hat <- list(Lambda_hat)
-    B_hat <- list(B_hat)
-    ll <- list(ll)
+   Lambda_hat <- list(Lambda_hat)
+   B_hat <- list(B_hat)
+   ll <- list(ll)
 
-    counter <- 1
-    Exit <- 1
+   counter <- 1
+   Exit <- 1
 
-    while(abs(Exit) > 0.01 & counter < 20){
+   while(abs(Exit) > 0.01 & counter < 20){
 
-      Sig1 <- solve(tcrossprod(B_hat[[counter]]))
-      Sig2 <- solve(B_hat[[counter]]%*%tcrossprod(Lambda_hat[[counter]], B_hat[[counter]]))
+     Sig1 <- solve(tcrossprod(B_hat[[counter]]))
+     Sig2 <- solve(B_hat[[counter]]%*%tcrossprod(Lambda_hat[[counter]], B_hat[[counter]]))
 
-      GLS1.1 <- rowSums(apply(Z_t[, 1:(TB-1)], 2, gls1, Sig = Sig1))
-      GLS1.2 <- rowSums(apply(Z_t[, (TB):ncol(Z_t)], 2, gls1, Sig = Sig2))
-      GLS1 <- solve(matrix(GLS1.1 + GLS1.2, nrow = k*k*p+k, byrow = F))
+     GLS1.1 <- rowSums(apply(Z_t[, 1:(TB-1)], 2, gls1, Sig = Sig1))
+     GLS1.2 <- rowSums(apply(Z_t[, (TB):ncol(Z_t)], 2, gls1, Sig = Sig2))
+     GLS1 <- solve(matrix(GLS1.1 + GLS1.2, nrow = k*k*p+k, byrow = F))
 
-      GLS2.1 <- matrix(0, nrow = k*k*p+k, ncol = (TB-1))
-      GLS2.2 <- matrix(0, nrow = k*k*p+k, ncol = ncol(y))
-
-
-      for(i in 1:(TB-1)){
-        GLS2.1[,i] <- kronecker(Z_t[,i], Sig1)%*%y[,i]
-      }
-      for(i in TB:ncol(Z_t)){
-        GLS2.2[,i] <- kronecker(Z_t[,i], Sig2)%*%y[,i]
-      }
-
-
-      GLS2.1 <- rowSums(GLS2.1)
-      GLS2.2 <- rowSums(GLS2.2)
-      GLS2 <- GLS2.1 + GLS2.2
-
-      GLS_hat <- GLS1%*%GLS2
-
-      term1 <- apply(Z_t, 2, resid.gls, k = k, GLS_hat = GLS_hat)
-      u_tgls <- t(y) - t(term1)
-
-      resid1gls <- u_tgls[1:TB-1,]
-      resid2gls <- u_tgls[TB:Tob,]
-      Sigma_hat1gls <- (crossprod(resid1gls)) / (TB-1)
-      Sigma_hat2gls <- (crossprod(resid2gls)) / (Tob-TB+1)
-
-      # Determine starting values for B and Lambda
-      MLEgls <- NULL
-      counter2 <- 0
-      while(is.null(MLEgls) & counter2 < 500){
-        MW <- -1
-        #MW2 <- -1
-        while(MW < 0.5){
-          B <- suppressMessages(expm::sqrtm((1/Tob)* crossprod(u_tgls))) + matrix(runif(k*k), nrow = k, byrow = T)
-          MW <- det(tcrossprod(B))
-          #MW2 <- det(B %*% tcrossprod(Psi, B))
-        }
-        #Lambda <- diag(Lambda_hat[[counter]])
-        S <- c(cbind(B, c(1,1,1)))
-
-        #optimize the likelihood function
-        MLEgls <- tryCatch(
-          optim(fn = LH, par = S, k = k, TB = TB, Sigma_hat1 = Sigma_hat1gls,
-                Sigma_hat2 = Sigma_hat2gls, Tob = Tob, method = 'L-BFGS-B', hessian = T),
-          error = function(e) NULL)
-        counter2 <- counter2 + 1
-        if(counter2 == 500){
-          cat('Algorithm does not converge')
-        }
-      }
+     GLS2.1 <- matrix(0, nrow = k*k*p+k, ncol = (TB-1))
+     GLS2.2 <- matrix(0, nrow = k*k*p+k, ncol = ncol(y))
 
 
-      B_hatg <- matrix(MLEgls$par[1:(k*k)], nrow = k)
-      Lambda_hatg <- diag(MLEgls$par[(k*k+1):(k*k+k)])
-      ll_g <- MLEgls$value
-
-      B_hat <- c(B_hat, list(B_hatg))
-      Lambda_hat <- c(Lambda_hat, list(Lambda_hatg))
-      ll <- c(ll, list(ll_g))
-
-      if(counter == 1){
-        GLSE <- list(GLS_hat)
-      }else{
-        GLSE <- c(GLSE, list(GLS_hat))
-      }
-
-      counter <- counter +1
-      #Exit <- sum(diag(Lambda_hat[[counter]])) - sum(diag(Lambda_hat[[counter - 1]]))
-      Exit <- ll[[counter]] - ll[[counter-1]]
-    }
-
-    # extracting the best estimates
-    ll <- unlist(ll)
-    llf <- ll[which.min(ll)]
-    cc <- which.min(ll)
-    B_hat <- B_hat[[cc]]
-    Lambda_hat <- Lambda_hat[[cc]]
-    GLSE <- GLSE[[cc-1]]
-    GLSE <- matrix(GLSE, nrow = k)
-
-    # obtaining standard errors from inverse fisher information matrix
-    HESS <- solve(MLEgls$hessian)
-
-    for(i in 1:nrow(HESS)){
-      if(HESS[i,i] < 0){
-        HESS[,i] <- -HESS[,i]
-      }
-    }
-
-    FishObs <- sqrt(diag(HESS))
-    B.SE <- matrix(FishObs[1:(k*k)], k, k)
-    Lambda.SE <- FishObs[(k*k+1):(k*k+k)]*diag(k)
+     for(i in 1:(TB-1)){
+       GLS2.1[,i] <- kronecker(Z_t[,i], Sig1)%*%y[,i]
+     }
+     for(i in TB:ncol(Z_t)){
+       GLS2.2[,i] <- kronecker(Z_t[,i], Sig2)%*%y[,i]
+     }
 
 
-    # ordering the columns with respect to the largest absolute values in each column
-    B_hat_ord <- matrix(0, k, k)
-    control <- rep(0, k)
-    for(i in 1:ncol(B_hat)){
-      for(j in 1:ncol(B_hat)){
-        if(which.max(abs(B_hat[, j])) == i){
-          if(control[i] == 0){
-            control[i] <- j
-            B_hat_ord[, i] <-B_hat[, j]
-          }else{
-            if(max(B_hat[, j]) > max(B_hat[, control[i]])){
-              control[i] <- j
-              B_hat_ord[, i] <-B_hat[, j]
-            }
-          }
-        }
-      }
-    }
+     GLS2.1 <- rowSums(GLS2.1)
+     GLS2.2 <- rowSums(GLS2.2)
+     GLS2 <- GLS2.1 + GLS2.2
 
-    # checking if any column in the orderd matrix is empty and replace it with the unused column in the estimated matrix
-    if(any(control == 0)){
-      hc <- sapply(1:k, function(x){any(x == control)})
-      B_hat_ord[, which(control == 0)] <- B_hat[, which(hc == FALSE)]
-      control[which(control == 0)] <- which(hc == FALSE)
-    }
+     GLS_hat <- GLS1%*%GLS2
 
-    # checking for negative values on the main daigonal
-    for(i in 1:ncol(B_hat_ord)){
-      if(B_hat_ord[i,i] < 0){
-        B_hat_ord[, i] <- B_hat_ord[, i]*(-1)
-      }
-      if(Lambda_hat[i,i] < 0){
-        Lambda_hat[, i] <- Lambda_hat[, i]*(-1)
-      }
-    }
+     term1 <- apply(Z_t, 2, resid.gls, k = k, GLS_hat = GLS_hat)
+     u_tgls <- t(y) - t(term1)
 
-    # reordering the lambda and S.E. matrices in the same way
-    B.SE <- B.SE[, control]
-    Lambda_hat <- diag(diag(Lambda_hat[, control]))
-    Lambda.SE <- diag(diag(Lambda.SE[, control]))
+     resid1gls <- u_tgls[1:TB-1,]
+     resid2gls <- u_tgls[TB:Tob,]
+     Sigma_hat1gls <- (crossprod(resid1gls)) / (TB-1)
+     Sigma_hat2gls <- (crossprod(resid2gls)) / (Tob-TB+1)
 
-    # Testing the estimated SVAR for identification by menas of wald statistic
-    wald <- wald.test(Lambda_hat, HESS)
+     # Determine starting values for B and Lambda
+     MLEgls <- NULL
+     counter2 <- 0
+     while(is.null(MLEgls) & counter2 < 500){
+       MW <- -1
+       #MW2 <- -1
+       while(MW < 0.5){
+         B <- suppressMessages(expm::sqrtm((1/Tob)* crossprod(u_tgls))) + matrix(runif(k*k), nrow = k, byrow = T)
+         MW <- det(tcrossprod(B))
+         #MW2 <- det(B %*% tcrossprod(Psi, B))
+       }
+       #Lambda <- diag(Lambda_hat[[counter]])
+       B <- c(B)
+       if(!is.null(restriction_matrix)){
+         restrictions <- length(restriction_matrix[!is.na(restriction_matrix)])
+         B <- B[1:(length(B)-restrictions)]
+       }
+       Lambda <- c(1,1,1)
+       S <- c(B, Lambda)
+
+       #optimize the likelihood function
+       MLEgls <- tryCatch(
+         optim(fn = LH, par = S, k = k, TB = TB, Sigma_hat1 = Sigma_hat1gls,
+               Sigma_hat2 = Sigma_hat2gls, Tob = Tob, method = 'L-BFGS-B', hessian = T, restriction_matrix = restriction_matrix),
+         error = function(e) NULL)
+       counter2 <- counter2 + 1
+       if(counter2 == 500){
+         cat('Algorithm does not converge')
+       }
+     }
+
+
+     B_hatg <- matrix(MLEgls$par[1:(k*k)], nrow = k)
+     Lambda_hatg <- diag(MLEgls$par[(k*k+1):(k*k+k)])
+     ll_g <- MLEgls$value
+
+     B_hat <- c(B_hat, list(B_hatg))
+     Lambda_hat <- c(Lambda_hat, list(Lambda_hatg))
+     ll <- c(ll, list(ll_g))
+
+     if(counter == 1){
+       GLSE <- list(GLS_hat)
+     }else{
+       GLSE <- c(GLSE, list(GLS_hat))
+     }
+
+     counter <- counter +1
+     #Exit <- sum(diag(Lambda_hat[[counter]])) - sum(diag(Lambda_hat[[counter - 1]]))
+     Exit <- ll[[counter]] - ll[[counter-1]]
+   }
+
+   # extracting the best estimates
+   ll <- unlist(ll)
+   llf <- ll[which.min(ll)]
+   cc <- which.min(ll)
+   B_hat <- B_hat[[cc]]
+   Lambda_hat <- Lambda_hat[[cc]]
+   GLSE <- GLSE[[cc-1]]
+   GLSE <- matrix(GLSE, nrow = k)
+
+   # obtaining standard errors from inverse fisher information matrix
+   HESS <- solve(MLEgls$hessian)
+
+   for(i in 1:nrow(HESS)){
+     if(HESS[i,i] < 0){
+       HESS[,i] <- -HESS[,i]
+     }
+   }
+
+   FishObs <- sqrt(diag(HESS))
+   B.SE <- matrix(FishObs[1:(k*k)], k, k)
+   Lambda.SE <- FishObs[(k*k+1):(k*k+k)]*diag(k)
+
+
+   # ordering the columns with respect to the largest absolute values in each column
+   B_hat_ord <- matrix(0, k, k)
+   control <- rep(0, k)
+   for(i in 1:ncol(B_hat)){
+     for(j in 1:ncol(B_hat)){
+       if(which.max(abs(B_hat[, j])) == i){
+         if(control[i] == 0){
+           control[i] <- j
+           B_hat_ord[, i] <-B_hat[, j]
+         }else{
+           if(max(B_hat[, j]) > max(B_hat[, control[i]])){
+             control[i] <- j
+             B_hat_ord[, i] <-B_hat[, j]
+           }
+         }
+       }
+     }
+   }
+
+   # checking if any column in the orderd matrix is empty and replace it with the unused column in the estimated matrix
+   if(any(control == 0)){
+     hc <- sapply(1:k, function(x){any(x == control)})
+     B_hat_ord[, which(control == 0)] <- B_hat[, which(hc == FALSE)]
+     control[which(control == 0)] <- which(hc == FALSE)
+   }
+
+   # checking for negative values on the main daigonal
+   for(i in 1:ncol(B_hat_ord)){
+     if(B_hat_ord[i,i] < 0){
+       B_hat_ord[, i] <- B_hat_ord[, i]*(-1)
+     }
+     if(Lambda_hat[i,i] < 0){
+       Lambda_hat[, i] <- Lambda_hat[, i]*(-1)
+     }
+   }
+
+   # reordering the lambda and S.E. matrices in the same way
+   B.SE <- B.SE[, control]
+   Lambda_hat <- diag(diag(Lambda_hat[, control]))
+   Lambda.SE <- diag(diag(Lambda.SE[, control]))
+
+   # Testing the estimated SVAR for identification by menas of wald statistic
+   wald <- wald.test(Lambda_hat, HESS)
+
 
   result <- list(
                  Lambda = Lambda_hat,    # estimated Lambda matrix (unconditional heteroscedasticity)
