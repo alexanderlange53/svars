8bbd550eb60c245d80df64220cfefd3b5ba4aa03
BDalheimer <bernhard.dalheimer@hotmail.com>
2017-05-01 20:32:26 +0200

    include lag helper function in main script

--- R/identifyVolatility.R
+++ R/identifyVolatility.R
@@ -47,6 +47,17 @@ ll <- MLE$value
 # estimating again with GLS to obatin a more precise estimation
 y <- t(x$y)
 
+y_lag_cr <- function(y, lag_length){
+  # create matrix that stores the lags
+  y_lag <- matrix(NA, dim(y)[1],dim(y)[2]*lag_length)
+  for (i in 1:lag_length) {
+    y_lag[(1+i):dim(y)[1],((i*NCOL(y)-NCOL(y))+1):(i*NCOL(y))] <- y[1:(dim(y)[1]-i),(1:NCOL(y))]
+  }
+  # drop first observation
+  y_lag <- as.matrix(y_lag[-(1:lag_length),])
+  out <- list(lags = y_lag)
+}
+
 yl <- t(y_lag_cr(t(y), p)$lags)
 y <- y[,-c(1:p)]
 
