dbd7e787ff09fc9deef1e6af9261aafe3660b17f
Alexander Lange <alexanderlange53@user.noreply@github.com>
2018-02-07 13:34:59 +0100

    continuoing smooth transition

--- R/id.st.R
+++ R/id.st.R
@@ -36,6 +36,23 @@ id.st <- function(x, c_lower = 0.25, c_upper = 0.75, c_step = 5,
     stop("Object class is not supported")
   }
 
+  # Function to create a block diagonal matrix
+  block.diagonal<-function(...){
+    matrixList <- list(...)
+    if(is.list(matrixList[[1]])) matrixList<-matrixList[[1]]
+
+    dimensions <- sapply(matrixList,FUN=function(x) dim(x)[1])
+    finalDimension <- sum(dimensions)
+    finalMatrix <- matrix(0,nrow=finalDimension,ncol=finalDimension)
+    index <- 1
+    for(k in 1:length(dimensions)){
+      finalMatrix[index:(index+dimensions[k]-1),index:(index+dimensions[k]-1)]<-matrixList[[k]]
+      index <- index+dimensions[k]
+    }
+    finalMatrix
+  }
+
+
   # Function for Z matrix
   y_lag_cr <- function(y, lag_length){
     # create matrix that stores the lags
@@ -60,14 +77,72 @@ id.st <- function(x, c_lower = 0.25, c_upper = 0.75, c_step = 5,
     return(G)
   }
 
+  # Likelihod function
+  likelihood.st <- function(parameter, u_t, G){
+
+    B <- matrix(parameter[1:(k*k)], k, k)
+    Lambda <- diag(parameter[(k*k+1):(k*k+k)])
+
+    Omega <- lapply(G, function(x, B, Lambda)(1 - x)*tcrossprod(B, B) + x*B%*%tcrossprod(Lambda, B),
+                    B = B, Lambda = Lambda)
+    Omega_inv <- lapply(Omega, function(x)solve(x))
+
+    Likelihood_part1 <- suppressWarnings(lapply(Omega, function(x) log(det(x))))
+    Likelihood_part1 <- Reduce('+', Likelihood_part1)
+
+    u_list <- apply(u_t, 1, list)
+
+    Likelihood_part2 <- sum(mapply(function(xx,yy){t(unlist(xx))%*%yy%*%unlist(xx)}, u_list, Omega_inv))
+
+    L <- -0.5*Likelihood_part1 - 0.5*Likelihood_part2
+
+    if(!is.na(L)){
+      return(-L) ;
+    }else{
+      return(1e25) ;
+    }
+
+  }
+
   # Creating grid for iterative procedure
   gamma_grid <-  seq(gamma_lower, gamma_upper, by = gamma_step)
   cc_grid <- seq(ceiling(c_lower*Tob), floor(c_upper*Tob), by = c_step)
   grid_comb <- unique(expand.grid(gamma_grid, cc_grid))
   G_grid <- mapply(transition, grid_comb[,1], grid_comb[,2], MoreArgs = list(st = seq(1:Tob)))
 
-  # Fiterative approach
-  iterative_smooth_transition <- function(transition, Tob, crit, max.iter){
+  # iterative approach
+  iterative_smooth_transition <- function(transition, u_t, y, Tob, crit, max.iter){
+
+    # Step 1: Optimizing likelihood with initial parameter
+    mle <- optim(fn = likelihood.st, par = parameter, u_t = u_t, G = transition, method = "BFGS")
+
+    B_hat <- matrix(mle$par[1:(k*k)], nrow = k)
+    Lambda_hat <- diag(mle$par[(k*k+1):(k*k+k)])
+    ll <- mle$value
+
+    # Step 2: Reestimation of VAR parameter with GLS
+    yl <- t(y_lag_cr(t(y), p)$lags)
+    yret <- y
+    y <- y[,-c(1:p)]
+
+    if(x$type == 'const'){
+      Z_t <- rbind(rep(1, ncol(yl)), yl)
+    }else if(x$type == 'trend'){
+      Z_t <- rbind(seq(1, ncol(yl)), yl)
+    }else if(x$type == 'both'){
+      Z_t <- rbind(rep(1, ncol(yl)), seq(1, ncol(yl)), yl)
+    }else{
+      Z_t <- yl
+    }
+
+    Omega_i <- lapply(transition, function(x, B, Lambda){solve((1 - x)*tcrossprod(B, B) + x*B%*%tcrossprod(Lambda, B))},
+                    B = B_hat, Lambda = Lambda_hat)
+
+    W <- blockMatrixDiagonal(Omega_i)
+
+    b_gls <- solve(kronecker(Z_t, diag(k))%*%W%*%kronecker(t(Z_t), diag(k)))%*%kronecker(Z_t, diag(k))%*%W%*%c(y)
+
+
 
     W <- solve(diag(K*(Tob))) # identity matrix as starting weighting matrix
 
@@ -174,16 +249,6 @@ id.st <- function(x, c_lower = 0.25, c_upper = 0.75, c_step = 5,
 
 
 
-  likelihood.st <- function(parameter, u_t){
 
-    B <- parameter[1:k*k]
-    Lambda <- parameter[(k*k+1):(k*k+k)]
-
-    Omega <- (1 - G)*tcrossprod(B, B) + G*B%*%tcrossprod(Lambda, B)
-
-    L <- -0.5*(log(det(Omega))) - 0.5*crossprod(u_t, Omega)%*%u_t
-
-
-  }
 
 }
