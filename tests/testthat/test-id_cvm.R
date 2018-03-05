b376170c892eb54dc05e980b7972d22ddfe30108
Alexander Lange <alex.lange_@gmx.de>
2018-03-05 11:49:56 +0100

    diverse test updates

---
+++ tests/testthat/test-id_cvm.R
@@ -0,0 +1,65 @@
+context("test-id_cvm.R")
+
+test_that("id.cvm 3-dims works", {
+  set.seed(23211)
+  v1 <- vars::VAR(USA, p = 6, ic = "AIC" )
+  cob <- copula::indepTestSim(v1$obs, v1$K, verbose=FALSE)
+  x1 <- id.cvm(v1, dd = cob)
+
+  expect_equal(sum(round(x1$B, 1)), 2.5)
+
+  expect_equal(x1$K, 3)
+  expect_equal(x1$n, 169)
+  expect_equal(x1$p, 6)
+
+  expect_lt(x1$test.stats, 0.014)
+
+  expect_match(x1$method, "Cramer-von Mises distance")
+})
+
+test_that("id.cvm 3-dims works with internal test stat calculation", {
+  set.seed(23211)
+  v1 <- vars::VAR(USA, p = 6, ic = "AIC" )
+  x1 <- id.cvm(v1, dd = NULL)
+
+  expect_equal(sum(round(x1$B, 1)), 2.5)
+
+  expect_equal(x1$K, 3)
+  expect_equal(x1$n, 169)
+  expect_equal(x1$p, 6)
+
+  expect_lt(x1$test.stats, 0.014)
+
+  expect_match(x1$method, "Cramer-von Mises distance")
+})
+
+test_that("id.cvm 2-dims works", {
+  set.seed(23211)
+  v1 <- vars::VAR(USA[,-3], p = 3, ic = "AIC" )
+  cob <- copula::indepTestSim(v1$obs, v1$K, verbose=FALSE)
+  x1 <- id.cvm(v1, dd = cob)
+
+  expect_equal(sum(round(x1$B, 1)), 0.3)
+
+  expect_equal(x1$K, 2)
+  expect_equal(x1$n, 172)
+  expect_equal(x1$p, 3)
+
+  expect_lt(x1$test.stats, 0.014)
+
+  expect_match(x1$method, "Cramer-von Mises distance")
+})
+
+test_that("id.cvm 2-dims works with internal test stat calculation", {
+  set.seed(23211)
+  v1 <- vars::VAR(USA[,-3], p = 3, ic = "AIC" )
+  x1 <- id.cvm(v1, dd = NULL)
+
+  expect_equal(x1$K, 2)
+  expect_equal(x1$n, 172)
+  expect_equal(x1$p, 3)
+
+  expect_lt(x1$test.stats, 0.014)
+
+  expect_match(x1$method, "Cramer-von Mises distance")
+})
