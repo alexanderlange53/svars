bb46aea8862e4649c6e759da973270da72bf3f3e
Bernhard Dalheimer <bernhard.dalheimer@hotmail.com>
2018-05-08 14:39:14 +0200

    Auto stash before merge of "TestableFunctions" and "origin/master"

--- R/plot_hd.R
+++ R/plot_hd.R
@@ -7,18 +7,17 @@ plot.hd <- function(x, ...){
 
   Index <- NULL
   value <- NULL
-
   PlotData <- as.data.frame(x$hidec)
-   if(inherits(x$hidec, "ts")){
-     tsStructure = attr(x$hidec, which = "tsp")
-     PlotData$Index <- seq(from = tsStructure[1], to = tsStructure[2], by = 1/tsStructure[3])
+  if(inherits(x$hidec, "ts")){
+    tsStructure = attr(x$hidec, which = "tsp")
+    PlotData$Index <- seq(from = tsStructure[1], to = tsStructure[2], by = 1/tsStructure[3])
 
-     PlotData$Index <- as.Date(yearmon(PlotData$Index))
+    PlotData$Index <- as.Date(yearmon(PlotData$Index))
 
-   } else {
-     PlotData$Index <- 1:nrow(PlotData)
-     PlotData$V1 <- NULL
-   }
+  } else {
+    PlotData$Index <- 1:nrow(PlotData)
+    PlotData$V1 <- NULL
+  }
 
   dat <- melt(PlotData,id = 'Index')
   ggplot(dat, aes(x = Index, y = value)) + geom_line() +
@@ -26,3 +25,5 @@ plot.hd <- function(x, ...){
     xlab("Time") + theme_bw()
 
 }
+
+
