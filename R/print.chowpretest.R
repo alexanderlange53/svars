e361d5ba2280cd61b239f02665f301e52656d8af
Bernhard Dalheimer <bernhard.dalheimer@hotmail.com>
2018-08-07 21:48:52 +0200

    fixed bug in print.chowpretest

--- R/print.chowpretest.R
+++ R/print.chowpretest.R
@@ -4,8 +4,8 @@ print.chowpretest <- function(x,...){
   cat("\nEmpirical Fluctuation Process: Multivariate Chow test\n\n")
   cat("From observation: ", x$from, "\n")
   cat("To observation: ", x$to, "\n")
-  cat("Maximum break point test statistic:", max(x$teststat_bp, na.rm = T), "at observation", which.max(na.omit(x$teststat_bp)), "\n")
-  cat("Maximum sample split test statistic:", max(x$teststat_sp, na.rm = T), "at observation", which.max(na.omit(x$teststat_sp)), "\n")
+  cat("Maximum break point test statistic:", max(x$teststat_bp, na.rm = T), "at observation", x$from + which.max(na.omit(x$teststat_bp)) + x$var$p -1, "\n")
+  cat("Maximum sample split test statistic:", max(x$teststat_sp, na.rm = T), "at observation", x$from + which.max(na.omit(x$teststat_sp)) + x$var$p -1, "\n")
   cat("\n")
 
 
