903233043693366e37c4aa6c1bfa5c019d80d3f7
Alexander Lange <alex.lange_@gmx.de>
2019-02-22 16:17:22 +0100

    start with restrictions

---
+++ R/identifyGARCH.R
@@ -0,0 +1,221 @@
+identifyGARCH <- function(B0, k, Tob, restriction_matrix, Sigma_e_univ, parameter_ini_univ, max.iter, crit, u, p, x, type, yOut, coef_x){
+  ## Stage 2: Multivariate optimization
+
+  if(!is.null(restriction_matrix)){
+    # normalize the inverse of B to have ones on main diagonal
+    naElements <- is.na(restriction_matrix)
+    naElements_inv <- !is.na(restriction_matrix)
+    B0[naElements_inv] <- restriction_matrix[naElements_inv]
+
+    B_0_inv <- solve(B0)
+    norm_inv <- solve(diag(diag(B_0_inv), k, k))
+    B_0_inv_norm <-  norm_inv %*% B_0_inv
+    B_0_norm <- solve(norm_inv %*% B_0_inv)
+    B_0_norm[naElements_inv] <- restriction_matrix[naElements_inv]
+
+    B_param_ini <- B_0_inv_norm [col(B_0_inv_norm ) != row(B_0_inv_norm )]
+    diag_elements <- 1/(diag(B_0_inv))
+    restrictions <- length(restriction_matrix[!is.na(restriction_matrix)])
+  }else{
+    # normalize the inverse of B to have ones on main diagonal
+    B_0_inv <- solve(B0)
+    norm_inv <- solve(diag(diag(B_0_inv), k, k))
+    B_0_inv_norm <-  norm_inv %*% B_0_inv
+    B_0_norm <- solve(norm_inv %*% B_0_inv)
+
+    B_param_ini <- B_0_inv_norm [col(B_0_inv_norm ) != row(B_0_inv_norm )]
+    diag_elements <- 1/(diag(B_0_inv))
+    restrictions <- 0
+  }
+
+  ini <- c(B_param_ini, diag_elements)
+
+  # create empty vectors and lists for results
+  gamma <- rep(NA, k)
+  g <-  rep(NA, k)
+  param <-  rep(NA, k)
+  results_B <- list()
+  results_param <- list()
+  round <-  1
+  Exit <- 1
+
+  B_inv <- diag(k)
+  ll <- list()
+  multi_ml <- list()
+  uni_ml <- list()
+  uni_single_ml <- list()
+
+
+  while (Exit > crit & round < max.iter){
+    max_ml <- nlm(ini, f = likelihood_garch_multi, k = k, Tob = Tob, restriction_matrix = restriction_matrix,
+                  Sigma_e = Sigma_e_univ , u = u, iterlim = 150, hessian = T)
+
+    multi_ml[[round]] <- max_ml
+    # initials for next round of univariate estimation
+    ini <- max_ml$estimate
+
+    if(!is.null(restriction_matrix)){
+      # get the B matrix from the estimated parameters
+      naElements <- is.na(restriction_matrix)
+      diag(naElements) <- FALSE
+      B_inv <- restriction_matrix
+      B_inv[naElements] <- ini[1:sum(naElements)]
+      diag(B_inv) <- 1
+      B <- solve(B_inv)
+
+      diagonal_mat <- diag(ini[(sum(naElements) + 1):(sum(naElements) + 3)])
+    }else{
+      # get the B matrix from the estimated parameters
+      B_inv[col(B_inv) != row(B_inv)] <- ini[1:(k^2-k)]
+      B <- solve(B_inv)
+
+      diagonal_mat <- diag(ini[(k^2-k+1):k^2])
+    }
+
+    B_est <- B %*% diagonal_mat
+    B_est_inv <- solve(B_est)
+    # save individual B matrices for each round
+    results_B[[round]] <- B_est
+    # calculate new structural residuals for update of the GARCH parameters
+    est_r <- B_est_inv %*% t(u)
+
+    # Evaluating exit criterion
+    ll[[round]] <- max_ml$minimum
+    if(round > 1){
+      Exit <- abs(ll[[round-1]] - max_ml$minimum)
+    }
+
+    # re-estimate GARCH part, based on update of estimate of B
+    # optimizing the univariate likelihood functions
+    maxL <- list()
+    gamma_univ <- rep(NA, k)
+    g_univ <- rep(NA, k)
+    param_univ <- matrix(NA, 3, k)
+    Sigma_e_univ <- matrix(NA, Tob, k)
+
+    # first observstion of strucutral variance is the estimated sample variance
+    Sigma_e_0 <-  matrix(diag(var(t(est_r))),  Tob, k, byrow = T)
+
+    for(i in 1:k){
+      maxL <- nlm(p = parameter_ini_univ[i, ], f = likelihood_garch_uni, k = k, Tob = Tob,
+                  Sigma_1 = Sigma_e_0[, i] , est = est_r[i, ], iterlim = 150, hessian = T)
+      uni_single_ml[[i]] <- maxL$hessian
+
+      gamma_univ[i] <- maxL$estimate[1]
+      g_univ[i] <- maxL$estimate[2]
+
+      param_univ[, i] <- rbind((1- gamma_univ[i]- g_univ[i]), gamma_univ[i], g_univ[i])
+      Sigma_e_univ[,i] <- sigma_garch_univ(param_univ[,i], Tob, Sigma_e_0[,i], est_r[i,])
+    }
+
+    uni_ml[[round]] <- uni_single_ml
+    results_param[[round]] <- parameter_ini_univ <-  cbind(gamma_univ, g_univ)
+    round <-  round + 1
+  } # end of while loop
+
+  # extracting the best estimates
+  ll <- unlist(ll)
+  llf <- ll[which.min(ll)]
+  # Calculate log likelihood with normalizing constant
+  llf <- log(sqrt(1/(2 * pi)^k)) * Tob - llf
+  cc <- which.min(ll)
+  B_hat <- results_B[[cc]]
+  GARCH_param_hat <- results_param[[cc]]
+
+  # Standard errors
+  multi_ml <- multi_ml[[cc]]
+  HESS <- solve(multi_ml$hessian)
+
+  uni_ml <- uni_ml[[cc]]
+  HESS_univ <- lapply(uni_ml, function(x) diag(solve(x)))
+  GARCH_SE <- do.call('rbind', HESS_univ)
+
+  for(i in 1:nrow(HESS)){
+    if(HESS[i,i] < 0){
+      HESS[,i] <- -HESS[,i]
+    }
+  }
+
+  FishObs <- sqrt(diag(HESS))
+  B_inv_SE <- matrix(NA, k, k)
+
+  if(!is.null(restriction_matrix)){
+    naElements <- is.na(restriction_matrix)
+    diag(naElements) <- FALSE
+    B_inv_SE <- restriction_matrix
+    B_inv_SE[naElements] <- FishObs[1:sum(naElements)]
+    diag(B_inv) <- 0
+
+    B_inv_diag_SE <- diag(FishObs[(sum(naElements) + 1):(sum(naElements) + k)])
+  }else{
+    B_inv_SE[col(B_inv_SE) != row(B_inv_SE)] <- FishObs[1:(k * k - k)]
+    diag(B_inv_SE) <- 0
+    B_inv_diag_SE <- diag(FishObs[(k * k - k + 1):(k * k)])
+  }
+
+  rownames(B_hat) <- colnames(u)
+  rownames(B_inv_SE) <- colnames(u)
+  colnames(GARCH_SE) <- colnames(GARCH_param_hat) <- c('gamma', 'g')
+
+  # obtaining VAR parameter
+  if(inherits(x, "var.boot")){
+    A_hat <- coef_x
+  }else{
+    A <- matrix(0, nrow = k, ncol = k*p)
+    for(i in 1:k){
+      A[i,] <- coef_x[[i]][1:(k*p),1]
+    }
+
+    A_hat <- A
+
+    if(type == "const"){
+      v <- rep(1, k)
+
+      for(i in 1:k){
+        v[i] <- coef_x[[i]][(k*p+1), 1]
+      }
+
+      A_hat <- cbind(v, A)
+    }else if (type == "trend"){
+      trend <- rep(1, k)
+
+      for(i in 1:k){
+        trend[i] <- coef_x[[i]][(k*p+1), 1]
+      }
+
+      A_hat <- cbind(trend, A)
+    }else if(type == "both"){
+      v <- rep(1, k)
+
+      for(i in 1:k){
+        v[i] <- coef_x[[i]][(k*p+1), 1]
+      }
+
+      trend <- rep(1, k)
+
+      for(i in 1:k){
+        trend[i] <- coef_x[[i]][(k*p+2), 1]
+      }
+
+      A_hat <- cbind(v, trend, A)
+    }
+  }
+
+  return(list(
+    B = B_hat,              # estimated B matrix (unique decomposition of the covariance matrix)
+    B_inv_SE = B_inv_SE,            # standard errors of B matrix
+    GARCH_parameter = GARCH_param_hat,
+    GARCH_SE  = GARCH_SE,
+    n = Tob,                # number of observations
+    Fish = HESS,            # observerd fisher information matrix
+    Lik = llf,             # function value of likelihood
+    iteration = round,     # number of gls estimations
+    method = "GARCH",
+    restrictions = restrictions, # number of restrictions
+    A_hat = A_hat,            # VAR parameter estimated with gls
+    type = type,          # type of the VAR model e.g 'const'
+    y = yOut,                # Data
+    p = unname(p),                # number of lags
+    K = k                 # number of time series
+  ))
+}
--- R/likelihood_garch.R
+++ R/likelihood_garch.R
@@ -8,7 +8,7 @@ likelihood_garch_uni <- function(parameter, est, Sigma_1, Tob, k){
   g      <-  parameter[2]
 
   # Checking for input validity
-  if(gamma > 0.001 & g >= 0.001 & gamma + g < 0.9999){
+  if(gamma > 0.001 & g >= 0.001 & gamma + g < 0.999){
 
     # Likelihood function
     Sigma_2 <- t(Sigma_1)
@@ -32,13 +32,30 @@ likelihood_garch_uni <- function(parameter, est, Sigma_1, Tob, k){
 ## Likelihood for multivariate GARCH model ##
 #===========================================#
 
-likelihood_garch_multi <-  function(parameter, Sigma_e, Tob, k, u){
+likelihood_garch_multi <-  function(parameter, Sigma_e, Tob, k, u, restriction_matrix){
 
-  B_inv <- diag(k)
-  B_inv[col(B_inv) != row(B_inv)] <- parameter[1: (k^2 - k)]
-  B_norm <-  solve(B_inv)
+  if(!is.null(restriction_matrix)){
+    if(!is.matrix(restriction_matrix)){
+      stop("Please provide a valid input matrix")
+    }
+    naElements <- is.na(restriction_matrix)
+    diag(naElements) <- FALSE
+    toFillMatrix <- restriction_matrix
+    toFillMatrix[naElements] <- parameter[1:sum(naElements)]
+    diag(toFillMatrix) <- 1
+    B_inv <- toFillMatrix
+    B_norm <-  solve(B_inv)
 
-  diag_comp <- diag(parameter[(k^2 - k + 1): (k^2)])
+    diag_comp <- diag(parameter[(sum(naElements) + 1): (sum(naElements) + k)])
+  }else{
+    B_inv <- diag(k)
+    B_inv[col(B_inv) != row(B_inv)] <- parameter[1: (k^2 - k)]
+    B_norm <-  solve(B_inv)
+
+    diag_comp <- diag(parameter[(k^2 - k + 1): (k^2)])
+  }
+
+
   # diag_comp is the inverted and squared diagonal elements of B_inv-matrix (i.e. (norm_inv)^2 ) = sqrt(diag_comp)
 
   B <- solve(solve(diag_comp)%*%solve(B_norm))
