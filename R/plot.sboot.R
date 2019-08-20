2876b46b9e003f61d3fb28314c476a115f061d1d
Alexander Ritz <alex.ritz92@googlemail.com>
2019-08-20 11:04:15 +0200

    Implemented choice of bonferroni confidence

--- R/plot.sboot.R
+++ R/plot.sboot.R
@@ -7,30 +7,64 @@ plot.sboot <- function(x, scales = "free_y", lowerq = 0.16, upperq = 0.84, perce
   horizon <- nrow(confidence[[1]]$irf)
   kk <- ncol(confidence[[1]]$irf)
   nboot <- length(confidence)
+  rest <- x$rest_mat
 
   intervals <- array(0, c(horizon, kk, nboot))
   for(i in 1:nboot){
     intervals[,,i] <- as.matrix(confidence[[i]]$irf)
   }
 
-  lower <- matrix(0, horizon, kk)
-  for(i in 1:horizon){
-    for(j in 1:kk){
-      lower[i,j] <- quantile(intervals[i,j,], probs = lowerq)
-    }
-  }
+  if(percentile == 'standard' | percentile == 'hall'){
+    lower <- matrix(0, horizon, kk)
+      for(i in 1:horizon){
+        for(j in 1:kk){
+          lower[i,j] <- quantile(intervals[i,j,], probs = lowerq)
+        }
+      }
 
-  upper <- matrix(0, horizon, kk)
-  for(i in 1:horizon){
-    for(j in 1:kk){
-      upper[i,j] <- quantile(intervals[i,j,], probs = upperq)
+
+    upper <- matrix(0, horizon, kk)
+    for(i in 1:horizon){
+      for(j in 1:kk){
+        upper[i,j] <- quantile(intervals[i,j,], probs = upperq)
+      }
     }
-  }
-  lower <- as.data.frame(lower)
-  upper <- as.data.frame(upper)
-  if(percentile == 'hall'){
-    lower <- 2*x$true$irf - lower
-    upper <- 2*x$true$irf - upper
+    lower <- as.data.frame(lower)
+    upper <- as.data.frame(upper)
+    if(percentile == 'hall'){
+      lower <- 2*x$true$irf - lower
+      upper <- 2*x$true$irf - upper
+    }
+  }else if(percentile == 'bonferroni'){
+    rest <- matrix(t(rest), nrow = 1)
+    rest[is.na(rest)] <- 1
+    rest[kk] <- 1
+    lower <- matrix(0, horizon, kk)
+    for(i in 1:horizon){
+      for(j in 1:kk){
+        if(rest[j] == 0){
+          lower[i,j] <- quantile(intervals[i,j,], probs = (lowerq/horizon))
+        } else{
+          lower[i,j] <- quantile(intervals[i,j,], probs = (lowerq/(horizon + 1)))
+          }
+      }
+    }
+
+
+    upper <- matrix(0, horizon, kk)
+    for(i in 1:horizon){
+      for(j in 1:kk){
+        if(rest[j] == 0){
+          upper[i,j] <- quantile(intervals[i,j,], probs = 1 + (((upperq - 1)/horizon)))
+        }else{
+          upper[i,j] <- quantile(intervals[i,j,], probs = 1 + (((upperq - 1)/(horizon + 1))))
+        }
+      }
+    }
+    lower <- as.data.frame(lower)
+    upper <- as.data.frame(upper)
+  }else {
+    print("Invalid choice of percentile; choose between standard, hall and bonferroni")
   }
 
   lower <- melt(lower, id = 'V1')
