30421436cd6d514c803c168b1140832ac9bb3486
Alexander Lange <alexander.lange@uni-goettingen.de>
2021-03-30 16:16:20 +0200

    bootstrap for 3 regimes implemented

--- R/id_st_boot.R
+++ R/id_st_boot.R
@@ -21,6 +21,12 @@ id.st_boot <- function(x, c_fix = NULL, transition_variable = NULL,
     return(G)
   }
 
+  transition_f2 <- function(gamma, cc, st){
+    G <- (1 + exp(exp(gamma)*(st - cc)))^(-1)
+    return(G)
+  }
+
+  if (length(gamma_fix) == 1) {
     grid_comb <- unique(expand.grid(gamma_fix, c_fix))
     if(is.null(transition_variable)){
       G_grid <- mapply(transition_f, grid_comb[,1], grid_comb[,2], MoreArgs = list(st = seq(1:Tob)))
@@ -30,6 +36,20 @@ id.st_boot <- function(x, c_fix = NULL, transition_variable = NULL,
       }
       G_grid <- mapply(transition_f, grid_comb[,1], grid_comb[,2], MoreArgs = list(st = transition_variable))
     }
+  } else if (length(gamma_fix) == 2) {
+    grid_comb1 <- unique(expand.grid(gamma_fix[1], c_fix[1]))
+    grid_comb2 <- unique(expand.grid(gamma_fix[2], c_fix[2]))
+    if(is.null(transition_variable)){
+      G_grid1 <- mapply(transition_f2, grid_comb1[,1], grid_comb1[,2], MoreArgs = list(st = seq(1:Tob)))
+      G_grid3 <- mapply(transition_f, grid_comb2[,1], grid_comb2[,2], MoreArgs = list(st = seq(1:Tob)))
+    }else{
+      if(length(transition_variable) != Tob){
+        stop('length of transition variable is unequal to data length')
+      }
+      G_grid1 <- mapply(transition_f2, grid_comb1[,1], grid_comb1[,2], MoreArgs = list(st = transition_variable))
+      G_grid3 <- mapply(transition_f, grid_comb2[,1], grid_comb2[,2], MoreArgs = list(st = transition_variable))
+    }
+  }
 
   if(is.null(Z)){
     yl <- t(YLagCr(t(y), p))
@@ -51,7 +71,7 @@ id.st_boot <- function(x, c_fix = NULL, transition_variable = NULL,
   }
 
 
-
+  if (exists('G_grid')) {
     best_estimation <- IterativeSmoothTransition(transition = G_grid, u = u, Tob = Tob, k = k, p = p,
                                                  crit = crit, maxIter = max.iter, Z_t = Z_t, Yloop = y_loop,
                                                  RestrictionMatrix = restriction_matrix, restrictions = restrictions)
@@ -61,6 +81,18 @@ id.st_boot <- function(x, c_fix = NULL, transition_variable = NULL,
     transition_coefficient <- gamma_fix
     SB <- c_fix
     comb <- 1
+  } else {
+    best_estimation <- IterativeSmoothTransition2(transition1 = G_grid1, transition2 = G_grid3, u = u, Tob = Tob, k = k, p = p,
+                                                  crit = crit, maxIter = max.iter, Z_t = Z_t, Yloop = y_loop,
+                                                  RestrictionMatrix = restriction_matrix, restrictions = restrictions)
+    transition_function <- G_grid1
+    transition_function3 <- G_grid3
+    transition_function2 <- 1 - transition_function - transition_function3
+
+    transition_coefficient <- c(gamma_fix[1], gamma_fix[2])
+    SB <- c(c_fix[1], c_fix[2])
+    comb <- 1
+  }
 
 
   result <- list(
